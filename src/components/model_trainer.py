7bfc601cbf1ea9179de34e18425867db51fe5e39
haffo <2146797+haffo@users.noreply.github.com>
2025-03-17 09:35:23 -0400

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,47 @@ class ModelTrainer:
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(x_train,y_train,x_test,y_test,models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error'],
+                    'max_depth': [None],
+                    'max_features': [None],
+                    'min_samples_split': [2],
+                    'random_state': [0]
+                },
+                "Random Forest":{
+                    # 'criterion': ['squared_error'],
+                    # 'max_depth': [None],
+                    'max_features': ['sqrt'],
+                    'min_samples_split': [2],
+                    'n_estimators': [100, 500]
+                },
+                "Gradient Boosting":{
+                    # 'loss': ['squared_error'],
+                    'learning_rate': [0.1, 0.01],
+                    'subsample': [1.0, 0.5],
+                    'n_estimators': [50, 100]    
+                },  
+                "Linear Regression":{}, 
+                "XGBRegressor":{
+                    'learning_rate': [0.1, 0.01],   
+                    'n_estimators': [8, 16]
+                }
+                ,
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate': [0.1, 0.01],
+                    'n_estimators': [50, 100]
+                }
+            }
+
+
+
+            model_report:dict=evaluate_models(x_train,y_train,x_test,y_test,models, params)
             print(model_report)
             print("\n====================================================================================\n")
             logging.info(f"Model Report : {model_report}")
@@ -60,7 +100,7 @@ class ModelTrainer:
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model on both training and testing dataset is: {best_model_name}")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
